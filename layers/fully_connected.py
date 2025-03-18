208ecf9291a1a65f34b66504a31808e0137e80c1
Nicholas <nicholas.scinocco2@gmail.com>
2025-03-18 15:54:16 -0400

    full dense layer implementation

---
+++ layers/fully_connected.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Layer_Dense:
+    def __init__(self, input_size, output_size):
+        self.weights = np.random.randn(input_size, output_size) * np.sqrt(2. / input_size)
+        self.biases = np.zeros((1, output_size))
+    
+    def forward(self, inputs):
+        self.inputs = inputs
+        return np.dot(inputs, self.weights) + self.biases
+
+    def backward(self, dvalues):
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        self.dinputs = np.dot(dvalues, self.weights.T)
